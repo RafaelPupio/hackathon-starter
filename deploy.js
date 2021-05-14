f70594fa6abe9b0a6c776178550898a8665eaa2d
Rafael <rafaelpupio@gmail.com>
2021-05-14 19:19:09 -0400

    Make Change

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '54.157.157.122',
+      host: '100.25.197.125',
       username: 'ubuntu',
       privateKey: 'HS-Key.pem'
     })
